9e781a19082f5b36ee36f025b9d84ae63721d99c
Samuel Setarige <setarigesamuel@gmail.com>
2026-03-24 10:54:32 +0300

    frontend ready

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = "https://blood-donation-and-bank-management-system.onrender.com/api/v1"
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const publicRequest = axios.create({
    baseURL: BASE_URL,

d298c16f07fbf352daade1cb3ca01f8d6ea5adc3
Samuel Setarige <setarigesamuel@gmail.com>
2026-03-28 16:33:55 +0300

    hallal

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,6 +1,3 @@
-
-
-
 import axios from 'axios'
 
 const BASE_URL = import.meta.env.VITE_API_URL;
@@ -8,35 +5,9 @@ console.log("API URL:", BASE_URL);
 
 export const publicRequest = axios.create({
    baseURL: BASE_URL,
-});
-
-// 🔥 ADD THIS
-publicRequest.interceptors.request.use((config) => {
-   const hospital = JSON.parse(localStorage.getItem("hospital"));
-   const hospital = JSON.parse(localStorage.getItem("hospital"));
-   const donor = JSON.parse(localStorage.getItem("donor"));
-   const admin = JSON.parse(localStorage.getItem("admin"));
-
-   const token =
-      hospital?.accessToken ||
-      donor?.accessToken ||
-      admin?.accessToken;
-
-   if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-   }
-
-   return config;
-});
+})
 
 export const adminRequest = axios.create({
    baseURL: "https://blood-donation-and-bank-management-system.onrender.com/api/v1/admin",
-});
-
-adminRequest.interceptors.request.use((config) => {
-   const admin = JSON.parse(localStorage.getItem("admin"));
-   if (admin?.accessToken) {
-      config.headers.Authorization = `Bearer ${admin.accessToken}`;
-   }
-   return config;
+   withCredentials: true,
 });
